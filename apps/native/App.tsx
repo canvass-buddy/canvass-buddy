0766e53a58b2157adc12c534a3490c34b0b0c0e9
Erik Badger <erikbadger777@gmail.com>
2023-01-19 10:00:14 -0600

    feat(native): added navigation packages

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,20 +1,52 @@
 import { StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Button } from "ui";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator, NativeStackNavigationProp, NativeStackScreenProps } from "@react-navigation/native-stack";
 
-export default function Native() {
+type RootStackParamList = {
+  Home:undefined,
+  Login:undefined,
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function HomeScreen({ navigation}:NativeStackScreenProps<RootStackParamList, 'Home'>) {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Native</Text>
       <Button
         onClick={() => {
           console.log("Pressed!");
-          alert("Pressed!");
+          navigation.push('Login')
+          // navigation.navigate('Login', {})
         }}
         text="Boop"
-      />
+        />
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+function LoginScreen({}:NativeStackScreenProps<RootStackParamList, 'Login'>) {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+  return (
+    <View>
+      <Button text="Login" onClick={() => {
+        navigation.push('Home');
+      }}/>
+    </View>
+  )
+}
+
+export default function Native() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
