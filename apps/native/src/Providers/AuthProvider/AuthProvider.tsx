0a963d6d16b7043f6d17f04a10d4daf449a783b0
Erik Badger <erikbadger777@gmail.com>
2023-02-09 10:36:51 -0600

    feat(native): added async storage to auth provider

--- apps/native/src/Providers/AuthProvider/AuthProvider.tsx
+++ apps/native/src/Providers/AuthProvider/AuthProvider.tsx
@@ -1,15 +1,18 @@
 import { useMutation, MutationFunction } from '@apollo/client';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {
   createContext,
   FC,
   PropsWithChildren,
   useContext,
+  useEffect,
   useState,
 } from 'react';
 import { gql } from '../../__generated__';
 
 interface AuthProviderState {
   token?: string;
+  isLoadingToken: boolean;
   login: MutationFunction<
     { auth: { token: string } },
     { email: string; password: string }
@@ -22,6 +25,7 @@ interface AuthProviderState {
 }
 
 const AuthContext = createContext<AuthProviderState>({
+  isLoadingToken: false,
   async login() {
     return {};
   },
@@ -51,13 +55,25 @@ const SIGN_UP_QUERY = gql(/* GraphQL */ `
 
 export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
   const [token, setToken] = useState<string | undefined>();
+  const [isLoadingToken, setIsLoadingToken] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoadingToken(true);
+      setToken((await AsyncStorage.getItem('token')) ?? undefined);
+      setIsLoadingToken(false);
+    })();
+  }, []);
+
   const [login] = useMutation(LOGIN_QUERY, {
-    onCompleted: ({ auth }) => {
+    onCompleted: async ({ auth }) => {
+      await AsyncStorage.setItem('token', auth.token);
       setToken(auth.token);
     },
   });
   const [signUp] = useMutation(SIGN_UP_QUERY, {
-    onCompleted: ({ auth }) => {
+    onCompleted: async ({ auth }) => {
+      await AsyncStorage.setItem('token', auth.token);
       setToken(auth.token);
     },
   });
@@ -67,7 +83,9 @@ export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ token, login, signUp, logout }}>
+    <AuthContext.Provider
+      value={{ token, isLoadingToken, login, signUp, logout }}
+    >
       {children}
     </AuthContext.Provider>
   );
