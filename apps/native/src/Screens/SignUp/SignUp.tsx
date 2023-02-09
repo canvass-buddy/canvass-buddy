03a362491d061dab86a6be8d763a59278c2be8f8
Erik Badger <erikbadger777@gmail.com>
2023-02-09 11:08:30 -0600

    feat(native): made sign up page functional

--- apps/native/src/Screens/SignUp/SignUp.tsx
+++ apps/native/src/Screens/SignUp/SignUp.tsx
@@ -11,23 +11,50 @@ import {
   Text,
   TopNavigation,
 } from '@ui-kitten/components';
+import { useFormik } from 'formik';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useAuth } from '../../Providers';
 import { RootStackParamList } from '../types';
 
 export function SignUp({
   navigation,
 }: NativeStackScreenProps<RootStackParamList, 'SignUp'>) {
   const onLogin = () => navigation.navigate('Login');
-  const onSignUp = () => navigation.navigate('Home');
+  // const onSignUp = () => navigation.navigate('Home');
+
+  const { t } = useTranslation();
+  const { signUp } = useAuth();
+
+  const f = useFormik({
+    initialValues: {
+      email: '',
+      name: '',
+      password: '',
+      passwordConfirm: '',
+    },
+    async onSubmit({ email, password, name }) {
+      await signUp({
+        variables: {
+          email,
+          password,
+          name,
+        },
+      });
+    },
+  });
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Layout style={{ flexGrow: 1 }}>
         <FillView alignX="center" alignY="center">
           <Text category="h1">Sign Up</Text>
           <Stack space={4} padding={4}>
-            <Input placeholder="Username" />
-            <Input placeholder="Password" />
+            <Input placeholder={t`auth.name`} />
+            <Input placeholder={t`auth.email`} />
+            <Input placeholder={t`auth.password`} />
+            <Input placeholder={t`auth.passwordConfirm`} />
             <Columns defaultWidth="1/2" space={4}>
               <Column>
                 <Button appearance="outline" status="basic" onPress={onLogin}>
@@ -35,7 +62,7 @@ export function SignUp({
                 </Button>
               </Column>
               <Column>
-                <Button onPress={onSignUp}>Sign Up</Button>
+                <Button onPress={() => f.handleSubmit()}>Sign Up</Button>
               </Column>
             </Columns>
           </Stack>
