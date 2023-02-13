3f20dead5730af09ebe23b0e00f5f08cc7d771a2
Erik Badger <erikbadger777@gmail.com>
2023-02-13 12:51:24 -0600

    fix(i18n): fixed localization error getting thrown in console

--- apps/native/src/i18n/index.ts
+++ apps/native/src/i18n/index.ts
@@ -3,6 +3,7 @@ import { initReactI18next } from 'react-i18next';
 import enUs from './locales/enUs.json';
 
 i18n.use(initReactI18next).init({
+  compatibilityJSON: 'v3',
   resources: {
     en: {
       translation: enUs,
